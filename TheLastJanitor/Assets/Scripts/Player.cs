c16f784b0c9cf425fa431a19ab6aeed168752e54
littlegamer757 <jneissl27@gmail.com>
2022-02-02 23:07:01 +0100

    Add Player Animations, UI Text > > Basic idle and running animations for the player character were added. Dabbled around with drawing text, but the results are not really satisfactory yet.

--- TheLastJanitor/Assets/Scripts/Player.cs
+++ TheLastJanitor/Assets/Scripts/Player.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-	private float speed = 2;
+	private float _speed = 2;
+	private float _stamina = 1;
+	private Animator _animator;
+	public Text _staminaText;
+
+	void Start()
+	{
+		_animator = GetComponent<Animator>();
+	}
 
 	void Update()
 	{
 		float x = Input.GetAxisRaw("Horizontal");
 		float y = Input.GetAxisRaw("Vertical");
 
-		// if (y != 0) x = y * 0.5f;
-
 		Vector2 direction = new Vector2(x, y).normalized;
+		_animator.SetFloat("Move X", x);
 
-		GetComponent<Rigidbody2D>().velocity = direction * speed;
+		GetComponent<Rigidbody2D>().velocity = direction * _speed * _stamina;
+
+		_stamina -= 1 / 100;
+		_staminaText.text = $"Stamina: {_stamina}";
+	}
+
+	void Eat()
+	{
+		_stamina = 1;
 	}
 }
